60e6b6e04e84a18ff435ade371d751ab7dc7a9cf
dikshaqa2023-lang <dikshaqa2023@gmail.com>
2026-09-23 17:34:32 +0530

    Update PurchaseTest.java

--- SauceDemoAutomation/SauceDemoAutomation/src/test/java/com/saucedemo/tests/PurchaseTest.java
+++ SauceDemoAutomation/SauceDemoAutomation/src/test/java/com/saucedemo/tests/PurchaseTest.java
@@ -4,4 +4,29 @@ import com.saucedemo.base.BaseTest;
 
 public class PurchaseTest extends BaseTest {
 
+   @Test
+    public void purchaseProduct() {
+        LoginPage loginPage = new LoginPage();
+        loginPage.login("standard_user", "secret_sauce");
+
+        ProductsPage productsPage = new ProductsPage();
+        Assert.assertEquals(productsPage.getProductsTitle(), "Products");
+
+        productsPage.addBackpackToCart();
+        productsPage.openCart();
+
+        CartPage cartPage = new CartPage();
+        Assert.assertTrue(cartPage.isBackpackDisplayed());
+
+        cartPage.clickCheckout();
+
+        CheckoutPage checkoutPage = new CheckoutPage();
+        checkoutPage.enterCustomerDetails("Saloni", "Sharma", "452001");
+        checkoutPage.clickContinue();
+        checkoutPage.clickFinish();
+
+        Assert.assertEquals(
+                checkoutPage.getSuccessMessage(),
+                "Thank you for your order!");
+    }
 }
